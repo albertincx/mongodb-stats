17dd2bd61fc03768180a89abadd57b5b1bfa4f37
Albert Safiullin <arsafiullin@maximum-avto.ru>
2019-03-21 15:43:02 +0300

    v1.0.4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoStat = require('./lib/stat');
+const defScheme = require('./stats.schema');
 
-module.exports = function (schema = {}) {
+module.exports = function (schema = defScheme) {
   return new MongoStat(schema);
 };
